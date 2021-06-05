386e8139162b778dbd3a117665aaf24b370ee73c
itskaustubh <kaustubhpb@gmail.com>
2021-06-05 15:48:54 +0530

    deploy to github pages

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'    ? '/personal_website/'    : '/',
     css: {
         loaderOptions: {
           sass: {
@@ -6,7 +7,7 @@ module.exports = {
           }
         }
       },
-      productionSourceMap: false, // Previously it was set as true
+      productionSourceMap: false, // Previously set as true
 }
 
 // https://github.com/vuejs-templates/webpack/blob/develop/template/config/index.js
