fc50cdb76ddda14d52e396c7e34d910de38fa0e5
itskaustubh <kaustubhpb@gmail.com>
2021-12-04 05:06:36 +0530

    feat: added prismic cms for projects

--- src/assets/content.js
+++ src/assets/content.js
@@ -1,186 +1,93 @@
-import { ref } from "@vue/reactivity"
+import { ref } from "@vue/reactivity";
 
-const projects = ref({
-    'odia_tts' : {
-        title : 'Odia Natural TTS',
-        thumbnail : 'odia_tts.jpg',
-        content : `Working on a TTS engine for Odia Language(Orissa) using Google's Tacotron 2 algorithm.`,
-        klink : 'http://ai4language.kaustubh.app/',
-        gitlink : 'https://itskaustubh.github.io/text-to-speech/',
-        link : '',
-        languages : ['Python','PyTorch','Svelte','ExpressJS'],
-        type : 'website'
-    },
-    'website' : {
-        title : 'Portfolio Website',
-        thumbnail : 'beta.jpg',
-        content : `You're on it right now. I'm really proud of how the website turned out, and I'm glad you took the time to check it out!`,
-        klink : 'https://kaustubh.app/',
-        gitlink : 'https://itskaustubh.github.io/',
-        link : '',
-        languages : ['VueJS','Firebase'],
-        type : 'website'
-    },
-    'neuboard' : {
-        title : 'Neuboard',
-        thumbnail : 'neuboard.jpg',
-        content : `A public message board to post messages, images and GIFs with a custom avatar maker.`,
-        gitlink : 'https://itskaustubh.github.io/neuboard/',
-        link : '',
-        languages : ['React','Firebase'],
-        type : 'website'
-    },
-    'basespace' : {
-        title : 'BaseSpace',
-        thumbnail : 'basespace.jpg',
-        content : `A website to help you browse through a list of warehouses to lease by applying different search filters.`,
-        klink : 'https://basespace.kaustubh.app',
-        gitlink : 'https://itskaustubh.github.io/vue-warehouse/',
-        link : '',
-        languages : ['VueJS'],
-        type : 'website'
-    },
-    'offlinetv' : {
-        title : 'OfflineTV Community',
-        thumbnail : 'offlinetv.jpg',
-        content : `A website dedicated to forming an online community for fans of the online streaming collective OfflineTV`,
-        klink : 'https://offlinetv.kaustubh.app',
-        gitlink : 'https://itskaustubh.github.io/offlineTV/',
-        link : '',
-        languages : ['VueJS'],
-        type : 'website'
-    },
-    'flutter_netflix' : {
-        title : 'Netflix Responsive App UI',
-        thumbnail : 'netflix.jpg',
-        content : 'Designed the Netflix Mobile and Web App using Flutter. Uses Video Player API to play videos on the Web Version.',
-        klink : 'https://flutter.kaustubh.app/netflix',
-        link : '',
-        languages: ['Flutter'],
-        type : 'website'
-    },
-    'flutter_food_delivery' : {
-        title : 'Food Delivery App UI',
-        thumbnail : 'food.jpg',
-        content : 'Food Delivery App with Cart, Menus and Restaurants.',
-        klink : 'https://flutter.kaustubh.app/food_delivery',
-        link : '',
-        languages: ['Flutter'],
-        type : 'website'
-    },
-    'flutter_budget' : {
-        title : 'Budget App UI',
-        thumbnail : 'budget.png',
-        content : 'Designed a Budget Planning App to keep track of my expenses. Features custom animations.',
-        klink : 'https://flutter.kaustubh.app/budget/',
-        link : '',
-        languages: ['Flutter'],
-        type : 'website'
-    },
-    'deep_dataset' : {
-        title : 'deepDataset',
-        thumbnail : 'deepDataset.jpg',
-        content : 'Grab image links from Google and DuckDuckGo to build an Image Dataset for Deep Learning or some other reason.',
-        link : 'https://addons.mozilla.org/en-US/firefox/addon/deepdataset/',
-        languages: ['JavaScript'],
-        type : 'extension'
-    },
-    'skribbl_plus' : {
-        title : 'Skribbl Plus',
-        thumbnail : 'skribbl.jpg',
-        content : 'Download Skribbl drawings with a single click. The days of janky screenshots is over.',
-        link : 'https://addons.mozilla.org/en-US/firefox/addon/skribbl-plus-save-drawings/',
-        languages: ['JavaScript'],
-        type : 'extension'
-    },
-    'pop_culture_dictionary' : {
-        title : 'Pop Culture Dictionary',
-        thumbnail : 'popCultureDictionary.jpg',
-        content : 'Easy to understand definitions for plain english words or phrases, fresh memes, emojis, pop culture references - get definitions for the latest lingo on the internet.',
-        link : 'https://addons.mozilla.org/en-US/firefox/addon/pop-culture-dictionary/',
-        languages: ['JavaScript'],
-        type : 'extension'
-    },
-    'portfolio_v1' : {
-        title : 'Old Portfolio Website',
-        thumbnail : 'portfolio_v1.jpg',
-        content : `Old archived version of the portfolio you're currently on.`,
-        klink : 'https://kaustubh.app/archives/portfolio_v1',
-        gitlink : '',
-        link : '',
-        languages: ['JavaScript','HTML','CSS',],
-        type : 'website'
-    },
-})
+import Prismic from "@prismicio/client";
 
+console.log(Prismic);
 
-// var temp = ''
+const projects = ref({});
+var apiEndpoint = "https://kaustubhportfolio.cdn.prismic.io/api/v2";
 
-// var callback = function(webLink){
-// 	temp = webLink
-//     console.log(temp)
-// }    
-
-// function checkWebsiteStatus(callback){
-//   callback && callback('hehe');
-// }
-
-// checkWebsiteStatus(callback)
-
-function setWebsiteLinks(key){
-    console.log('setting web links')
-    for(const project in projects.value){
-        if(projects.value[project].type == 'website' && projects.value[project][key] !== undefined && projects.value[project][key].length > 0){
-            projects.value[project].link = projects.value[project][key] 
-            // console.log(`${project}  :  ${projects.value[project].link}`)
-        }
+function setWebsiteLinks(key) {
+  console.log("setting web links");
+  for (const project in projects.value) {
+    if (
+      projects.value[project].type == "website" &&
+      projects.value[project][key] !== undefined &&
+      projects.value[project][key].length > 0
+    ) {
+      projects.value[project].link = projects.value[project][key];
+      // console.log(`${project}  :  ${projects.value[project].link}`)
     }
+  }
 }
 
-const websitesArray = [{
-    url : 'https://kaustubh.app/',
-    xkey : 'klink' 
-},{
-    url : 'https://itskaustubh.github.io/',
-    xkey : 'gitlink' 
-}]
-
+const websitesArray = [
+  {
+    url: "https://kaustubh.app/",
+    xkey: "klink",
+  },
+  {
+    url: "https://itskaustubh.github.io/",
+    xkey: "gitlink",
+  },
+];
 
 function checkWebsiteStatus(index = 0) {
-    var image = new Image();
-    image.onload = function() {
-      if (this.width > 0) {
-        console.log(`${websitesArray[index].url} is online!`);
-        setWebsiteLinks(websitesArray[index].xkey)
-      }
+  var image = new Image();
+  image.onload = function() {
+    if (this.width > 0) {
+      console.log(`${websitesArray[index].url} is online!`);
+      setWebsiteLinks(websitesArray[index].xkey);
     }
-    image.onerror = function() {
-      console.log(`${websitesArray[index].url} might be down`);
-      if( websitesArray.length !== (index + 1) ){
-          checkWebsiteStatus(index + 1)
-      }
+  };
+  image.onerror = function() {
+    console.log(`${websitesArray[index].url} might be down`);
+    if (websitesArray.length !== index + 1) {
+      checkWebsiteStatus(index + 1);
     }
-    image.src = websitesArray[index].url + 'favicon.ico';
+  };
+  image.src = websitesArray[index].url + "favicon.ico";
 }
-
-checkWebsiteStatus()
-
 
 const skills = {
-    'Python' : 'python.svg',
-    'C++' : 'c++.svg',
-    'Java'  :   'java.svg',
-    'JavaScript' : 'javascript.svg',
-    'React' : 'react.svg',
-    'VueJS' : 'vuejs.svg',
-    'Svelte' : 'svelte.svg',
-    'ExpressJS' : 'expressjs.svg',
-    'Flutter' : 'flutter.svg',
-    'HTML/CSS/SCSS' : 'html.svg',
-    'Firebase' : 'firebase.svg'
-}
+  Python: "python.svg",
+  "C++": "c++.svg",
+  Java: "java.svg",
+  JavaScript: "javascript.svg",
+  React: "react.svg",
+  VueJS: "vuejs.svg",
+  Svelte: "svelte.svg",
+  ExpressJS: "expressjs.svg",
+  Flutter: "flutter.svg",
+  "HTML/CSS/SCSS": "html.svg",
+  Firebase: "firebase.svg",
+};
 
-export {
-    projects,
-    skills
-}
+// fetching data from CMS
+const client = Prismic.client(apiEndpoint);
+client
+  .query("")
+  .then((resp) => {
+    const results = resp.results;
+    console.log(results);
+    results.forEach((item) => {
+      const data = item.data;
+      projects.value[item.uid] = {
+        title: data.title[0].text,
+        thumbnail: data.thumbnail.url,
+        content: data.content[0].text,
+        klink: data.klink.url === undefined ? "" : data.klink.url,
+        gitlink: data.gitlink.url === undefined ? "" : data.gitlink.url,
+        link: "",
+        languages: data.languages[0].text.split(","),
+        type: data.type[0].text,
+      };
+    });
+    console.log(projects.value);
+    checkWebsiteStatus();
+  })
+  .catch((err) => {
+    console.log("CMS fetch failed: ", err);
+  });
+
+export { projects, skills };
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,177 +1,226 @@
 <template>
-  <section class="s1">
-        <div class="main-container">
-            <h4 style="text-align: center">Some of my recent exploits</h4>
-            <div class="post-wrapper">
-                    <div class="post" v-for='project in projects' :key='project'>
-                        <!-- <ExpandableImage :src="require('../assets/images/' + project.thumbnail)" /> -->
-                        <div class="images" v-viewer.static="imageViewerOpts">
-                            <img class="thumbnail" :src="require('../assets/images/' + project.thumbnail)">
-                        </div>
-                        <!-- <expandable-image :src="require('../assets/images/' + project.thumbnail)"/> -->
-                        <div class="post-preview">
-                            
-                            <h6 class="post-title">{{project.title}}</h6>
-                            <p class="post-content">{{project.content}}</p>
-                            <div class='post-lang'>
-                                <span v-for="lang in project.languages" :key='lang' v-tooltip="lang" >
-                                    <img class="lang-icon" :src="require('../assets/icons/skills/' + lang.toLowerCase() + '.svg')"/>
-                                </span>
-                            </div>
-                            <div v-if="project.link != ''"> 
-                                <p><a class="post-link" target="_blank" :href="project.link">Check Out</a></p>
-                            </div>
-                        </div>
-                    </div>
+  <h4 style="text-align: center" v-if="Object.keys(projects).length == 0">
+    Fetching Projects...
+  </h4>
+  <section class="s1" v-if="Object.keys(projects).length !== 0">
+    <div class="main-container">
+      <h4 style="text-align: center">Some of my recent exploits</h4>
+
+      <div class="post-wrapper">
+        <div class="post" v-for="project in projects" :key="project">
+          <!-- <ExpandableImage :src="require('../assets/images/' + project.thumbnail)" /> -->
+          <div class="images" v-viewer.static="imageViewerOpts">
+            <img class="thumbnail" :src="project.thumbnail" />
+          </div>
+          <!-- <expandable-image :src="require('../assets/images/' + project.thumbnail)"/> -->
+          <div class="post-preview">
+            <h6 class="post-title">{{ project.title }}</h6>
+            <p class="post-content">{{ project.content }}</p>
+            <div class="post-lang">
+              <span
+                v-for="lang in project.languages"
+                :key="lang"
+                v-tooltip="lang"
+              >
+                <img
+                  class="lang-icon"
+                  :src="
+                    require('../assets/icons/skills/' +
+                      lang.toLowerCase() +
+                      '.svg')
+                  "
+                />
+              </span>
             </div>
+            <div v-if="project.link != ''">
+              <p>
+                <a class="post-link" target="_blank" :href="project.link"
+                  >Check Out</a
+                >
+              </p>
+            </div>
+          </div>
         </div>
-    </section>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-import { ref,onMounted } from 'vue'
-import {windows} from '../store/state'
-import {projects,skills} from '../assets/content'
+import { ref, onMounted, watch } from "vue";
+import { windows } from "../store/state";
+import { projects, skills } from "../assets/content";
 
 export default {
-    setup(){
-
-        const projectsState = ref(windows.value['projects'])
-        onMounted(() =>{
+  setup() {
+    const projectsState = ref(windows.value["projects"]);
+    onMounted(() => {
+      const unwatchProjects = watch(
+        () => projects.value.website,
+        () => {
+          console.log(Object.keys(projects).length);
+          if (Object.keys(projects).length !== 0) {
             projectsState.value.loaded = true;
-        })
-
-        const imageViewerOpts =  { "button": true, "navbar": false, "title": false, "toolbar": false, 
-                                    "tooltip": false, "movable": true, "zoomable": true, "rotatable": false, "scalable": true, 
-                                    "transition": true, "keyboard": false} 
-
-
-        return {
-            projects,
-            skills,
-            imageViewerOpts
+            unwatchProjects();
+          }
         }
-    }
-}
+      );
+    });
+
+    const imageViewerOpts = {
+      button: true,
+      navbar: false,
+      title: false,
+      toolbar: false,
+      tooltip: false,
+      movable: true,
+      zoomable: true,
+      rotatable: false,
+      scalable: true,
+      transition: true,
+      keyboard: false,
+    };
+
+    return {
+      projects,
+      skills,
+      imageViewerOpts,
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-
-*{
-    font-family: 'Open Sans','sans-serif';
-
+* {
+  font-family: "Open Sans", "sans-serif";
 }
 
-h1, h2, h3, h4, h5, h6, strong {
-    color: #000000;
-    font-weight: 500;
-    font-family: 'Open Sans','sans-serif';
-
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+strong {
+  color: #000000;
+  font-weight: 500;
+  font-family: "Open Sans", "sans-serif";
 }
 
-p, li, span, label, input, textarea {
-    color: #4b5156;
-    font-family: 'Open Sans','sans-serif';
+p,
+li,
+span,
+label,
+input,
+textarea {
+  color: #4b5156;
+  font-family: "Open Sans", "sans-serif";
 }
 
-
-ul{
-    list-style      :   none;
+ul {
+  list-style: none;
 }
 
-.s1{
-    background-color    :   white;
-    border-bottom       :   1px solid #c1c1c1;
-    overflow            :   auto;
-    padding-bottom: 10px;
+.s1 {
+  background-color: white;
+  border-bottom: 1px solid #c1c1c1;
+  overflow: auto;
+  padding-bottom: 10px;
 }
 
-
-
-h1  {font-size      :   56px;}
-h2  {font-size      :   36px;}
-h3  {font-size      :   32px;}
-h4  {font-size      :   24px;}
-h5  {font-size      :   20px;}
-h6  {font-size      :   16px;}
-
-
-.main-container{
-    color   :   white;
-    width   :   auto;
-    margin  :   0 auto;
+h1 {
+  font-size: 56px;
+}
+h2 {
+  font-size: 36px;
+}
+h3 {
+  font-size: 32px;
+}
+h4 {
+  font-size: 24px;
+}
+h5 {
+  font-size: 20px;
+}
+h6 {
+  font-size: 16px;
 }
 
-
-.post-wrapper{
-    display                 :   grid;
-    grid-template-columns   :   repeat(auto-fit, 320px);
-    gap                     :   10px;
-    justify-content         :   center ;
-
-    padding-bottom          :   30px;
+.main-container {
+  color: white;
+  width: auto;
+  margin: 0 auto;
 }
 
-.thumbnail{
-    display     :   block;
-    width       :   200px;
-    height      :   200px;
-    object-fit  :   contain;
+.post-wrapper {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, 320px);
+  gap: 10px;
+  justify-content: center;
 
-    margin-top      :   20px;
-    margin-left     :   auto;
-    margin-right    :   auto;
-
-    border              :   1px solid #eaeaea;
-    border-radius   :   5px;
+  padding-bottom: 30px;
 }
 
-.post{
-    position: relative;
+.thumbnail {
+  display: block;
+  width: 200px;
+  height: 200px;
+  object-fit: contain;
 
-    background-color    :   #fff;
-    padding             :   15px;
+  margin-top: 20px;
+  margin-left: auto;
+  margin-right: auto;
 
-    border          :   1px solid #eaeaea;
-    border-radius   :   5px;
+  border: 1px solid #eaeaea;
+  border-radius: 5px;
 }
 
-.post-title{
-    font-size   :   22px;
-    border-bottom: 1px solid #000;
-    padding-bottom: 3px;
-    border-color: #eaeaea;
+.post {
+  position: relative;
 
-    color       :   #111;
+  background-color: #fff;
+  padding: 15px;
 
-    display     :   block;
-    text-align  :   center;
-
-    margin-top      :   20px;
-    margin-bottom   :   20px;
+  border: 1px solid #eaeaea;
+  border-radius: 5px;
 }
 
-.post-content{
-    color           :   #4b5156;
-    font-size       :   20px;
-    margin-bottom   :   0px;
+.post-title {
+  font-size: 22px;
+  border-bottom: 1px solid #000;
+  padding-bottom: 3px;
+  border-color: #eaeaea;
+
+  color: #111;
+
+  display: block;
+  text-align: center;
+
+  margin-top: 20px;
+  margin-bottom: 20px;
 }
 
-.post-link{
-    position: absolute;
-    bottom: 5px;    
-    font-size: 20px;
+.post-content {
+  color: #4b5156;
+  font-size: 20px;
+  margin-bottom: 0px;
 }
 
-.post-lang{
-    display: inline-block;
-    padding-left    : 24px;
-
-    .lang-icon{
-        width   : 30px;
-        height  : 30px;
-        margin-right : 1px;
-    }
+.post-link {
+  position: absolute;
+  bottom: 5px;
+  font-size: 20px;
 }
 
+.post-lang {
+  display: inline-block;
+  padding-left: 24px;
+
+  .lang-icon {
+    width: 30px;
+    height: 30px;
+    margin-right: 1px;
+  }
+}
 </style>
