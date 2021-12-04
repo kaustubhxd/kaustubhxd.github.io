98d97ecfb408c547bfda35b6e63978808821b4cd
itskaustubh <kaustubhpb@gmail.com>
2021-12-05 04:22:29 +0530

    feat: crawler bot detection

--- src/scripts/clientAdditionalInfo.js
+++ src/scripts/clientAdditionalInfo.js
@@ -2,6 +2,9 @@ import { DISCORD_WEBHOOK_LINK } from "../store/keys";
 import Bowser from "bowser";
 
 let INFO = [];
+let flags = {
+  isCrawler: false,
+};
 
 export const getAdditionalInfo = () => {
   getBrowserInfo();
@@ -77,6 +80,14 @@ const getBrowserInfo = () => {
       value: `${battery.level * 100}% ${battery.charging ? "⚡" : ""}`,
     });
   });
+
+  let botPattern =
+    "(googlebot/|bot|Googlebot-Mobile|Googlebot-Image|Google favicon|Mediapartners-Google|bingbot|slurp|java|wget|curl|Commons-HttpClient|Python-urllib|libwww|httpunit|nutch|phpcrawl|msnbot|jyxobot|FAST-WebCrawler|FAST Enterprise Crawler|biglotron|teoma|convera|seekbot|gigablast|exabot|ngbot|ia_archiver|GingerCrawler|webmon |httrack|webcrawler|grub.org|UsineNouvelleCrawler|antibot|netresearchserver|speedy|fluffy|bibnum.bnf|findlink|msrbot|panscient|yacybot|AISearchBot|IOI|ips-agent|tagoobot|MJ12bot|dotbot|woriobot|yanga|buzzbot|mlbot|yandexbot|purebot|Linguee Bot|Voyager|CyberPatrol|voilabot|baiduspider|citeseerxbot|spbot|twengabot|postrank|turnitinbot|scribdbot|page2rss|sitebot|linkdex|Adidxbot|blekkobot|ezooms|dotbot|Mail.RU_Bot|discobot|heritrix|findthatfile|europarchive.org|NerdByNature.Bot|sistrix crawler|ahrefsbot|Aboundex|domaincrawler|wbsearchbot|summify|ccbot|edisterbot|seznambot|ec2linkfinder|gslfbot|aihitbot|intelium_bot|facebookexternalhit|yeti|RetrevoPageAnalyzer|lb-spider|sogou|lssbot|careerbot|wotbox|wocbot|ichiro|DuckDuckBot|lssrocketcrawler|drupact|webcompanycrawler|acoonbot|openindexspider|gnam gnam spider|web-archive-net.com.bot|backlinkcrawler|coccoc|integromedb|content crawler spider|toplistbot|seokicks-robot|it2media-domain-crawler|ip-web-crawler.com|siteexplorer.info|elisabot|proximic|changedetection|blexbot|arabot|WeSEE:Search|niki-bot|CrystalSemanticsBot|rogerbot|360Spider|psbot|InterfaxScanBot|Lipperhey SEO Service|CC Metadata Scaper|g00g1e.net|GrapeshotCrawler|urlappendbot|brainobot|fr-crawler|binlar|SimpleCrawler|Livelapbot|Twitterbot|cXensebot|smtbot|bnf.fr_bot|A6-Indexer|ADmantX|Facebot|Twitterbot|OrangeBot|memorybot|AdvBot|MegaIndex|SemanticScholarBot|ltx71|nerdybot|xovibot|BUbiNG|Qwantify|archive.org_bot|Applebot|TweetmemeBot|crawler4j|findxbot|SemrushBot|yoozBot|lipperhey|y!j-asr|Domain Re-Animator Bot|AddThis)";
+  let isCrawlerRx = new RegExp(botPattern, "i");
+  if (isCrawlerRx.test(navigator.userAgent)) {
+    flags.isCrawler = true;
+  }
+
   console.log(INFO);
 };
 
@@ -92,7 +103,10 @@ function sendToDiscord(addressLink) {
       embeds: [
         {
           color: 171159,
-          author: { name: "Anonymous", url: addressLink },
+          author: {
+            name: flags.isCrawler ? "Crawler" : "Anonymous",
+            url: addressLink,
+          },
           // title: "Message",
           thumbnail: {
             url:
